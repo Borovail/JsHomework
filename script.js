e37e8a381717ca538bfd2a123f38059b1f10f299
Borovail <skorikdaniil17@gmail.com>
2023-10-04 15:20:12 +0300

    3 Task

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("registrationForm");
-    const confirmation = document.getElementById("confirmation");
-    
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      
-      const email = form.email.value;
-      const password = form.password.value;
-      const confirmPassword = form.confirmPassword.value;
-      
-      if (password !== confirmPassword) {
-        confirmation.textContent = "Паролі не співпадають!";
-        return;
-      }
-      
-      confirmation.textContent = `На ${email} надіслано лист із підтвердженням.`;
-    });
-  });
+  const form = document.getElementById("userInfoForm");
+  const output = document.getElementById("output");
   
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const firstname = form.firstname.value;
+    const lastname = form.lastname.value;
+    const birthday = form.birthday.value;
+    const gender = form.gender.value;
+    const skills = form.skills.value;
+    
+    output.innerHTML = `
+      <strong>Ім'я:</strong> ${firstname} <br>
+      <strong>Прізвище:</strong> ${lastname} <br>
+      <strong>Дата народження:</strong> ${birthday} <br>
+      <strong>Стать:</strong> ${gender} <br>
+      <strong>Навички:</strong> ${skills} <br>
+    `;
+  });
+});
